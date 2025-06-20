a864ff225384e3dc44efad6e2c39f394ab19f17e
Pavel Katunin <katunin.pavel@gmail.com>
2025-06-20 10:29:53 +0300

    init

--- frontend/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import type {FC} from "react";
-
-export const Footer:FC = () => {
-    return (
-        <footer className="text-center container py-8">
-            <a className="hover:bg-blue-500 text-blue-700" href="https://ikatunin.ru" target='_blank'>ikatunin.ru</a>
-        </footer>
-    )
-}
--- frontend/src/router/privateRoute.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import {AuthService} from "../services/auth.service.ts";
-
-export const PrivateRoute = () => {
-    return !!AuthService.token ? <Outlet /> : <Navigate to="/login" replace />;
-};
